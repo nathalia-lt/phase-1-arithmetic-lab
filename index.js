a87e479d5c8862c79ab6b7aafcd40b03c6a05a8a
nathalia <nathaliatroina@gmail.com>
2022-01-16 15:47:55 -0500

    commit tree 188e22a6c54d15b3bfd292761eef25fb7092acb9

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
+
+
+const ceilRand = Math.ceil(Math.random() * 1000000)
+//math random creates a value between 0 and 1 and I ceil it
+//rounding it up. (I converted the random number into interger)
+const random = ceilRand
+// the test is asking for a varible to be named random, 
+//Im just coping the value
+
+
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4;
+
+const numbers = (7, 8, 9, 10, 20);
+const max = Math.max(numbers)
